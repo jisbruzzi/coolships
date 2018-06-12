a9ee1b87867636f90ca06515828f1c3de01159cd
jisbruzzi <jose.sbru@gmail.com>
2018-06-12 08:50:59 -0300

    empiezo a armar un test del posicionador

---
+++ posicionadorGreedy-test.js
@@ -0,0 +1,9 @@
+const assert=requre("chai").assert
+const posicionador=require("../posicionadorGreedy.")
+const barco = require("../barco")
+const vulnerabilidades=require("../vulnerabilidades")
+
+describe("Probando salidas de PosiciondorGreedy",function(){
+	it("todos iguales",function(){
+	})
+})
